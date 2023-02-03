414e5b9682166a4aac71d57cf1a16b14b0deeee6
Dang CJ <53930227+CjConvento@users.noreply.github.com>
2023-02-03 13:41:01 +0800

    Update run.sh
    Signed-off-by: Dang CJ <53930227+CjConvento@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
-http-server -p 8080 &
+http-server -p 8383 &
 python3 telegram_gcloner/telegram_gcloner.py
